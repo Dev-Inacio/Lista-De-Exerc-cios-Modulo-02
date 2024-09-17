45dd8ad83159c4f51aa16da605b83653bd89f9ae
DevInacio <joseinacio0398@gmail.com>
2024-09-16 22:10:21 -0300

    ajuste de renomeamento de pasta

--- exercicio04.js
+++ exercicio04.js
@@ -1,24 +1,24 @@
-
 // criei o arrey de objetos
+//de pessoas
 let pessoas = [
-    {
+  {
     nome: "inacio",
-    idade: "23", 
-    cidade: "santa rita"
-    },
-    {
+    idade: "23",
+    cidade: "santa rita",
+  },
+  {
     nome: "jaques",
-    idade: "26", 
-    cidade: "porto alegre"
-    }, 
-    {
+    idade: "26",
+    cidade: "porto alegre",
+  },
+  {
     nome: "edu",
-    idade: "27", 
-    cidade: "porto alegre"
-    }    
-]
+    idade: "27",
+    cidade: "porto alegre",
+  },
+];
 
 // iterei e mostrei no console
-for(let key of pessoas){
-    console.log(key)
+for (let key of pessoas) {
+  console.log(key);
 }
